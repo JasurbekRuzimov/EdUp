76365ce58f540cc2f8ff3b034c96d889c5e5d05a
Ruzimov Jasurbek <ruzimovjasur2105@gmail.com>
2022-08-25 12:39:21 +0500

    Some changes

--- app/src/main/java/uz/pdp/myeduapp/MainActivity.java
+++ app/src/main/java/uz/pdp/myeduapp/MainActivity.java
@@ -3,14 +3,7 @@ package uz.pdp.myeduapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.Window;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,20 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- //       Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorActionBar)));
 
-//    if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
-//        Window window=getWindow();
-//        window.setStatusBarColor(getResources().getColor(R.color.colorStatusBar));
-//    }
-//
-//    new Handler().postDelayed(new Runnable() {
-//        @Override
-//        public void run() {
-//            Intent intent=new Intent(MainActivity.this,SplashScreen.class );
-//            startActivity(intent);
-//            finish();
-//        }
-//    },5000);
     }
 }
