c13ba140f9ff11cd196daa0f86e701476bfdc7f6
JasurbekRuzimov <ruzimovjasur2105@gmail.com>
2022-09-23 14:43:43 +0500

    All codes were pushed

--- app/src/main/java/uz/pdp/myeduapp/Dasturlash_Activity.java
+++ app/src/main/java/uz/pdp/myeduapp/Dasturlash_Activity.java
@@ -1,8 +1,13 @@
 package uz.pdp.myeduapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Dasturlash_Activity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class Dasturlash_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dasturlash);
+        DrawerLayout drawerLayout=findViewById(R.id.drawerLayout);
+        findViewById(R.id.navbaricon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        NavigationView navigationView = findViewById(R.id.navBarMenu);
+        navigationView.setItemIconTintList(null);
     }
 }
--- app/src/main/java/uz/pdp/myeduapp/MainActivity.java
+++ app/src/main/java/uz/pdp/myeduapp/MainActivity.java
@@ -1,21 +1,28 @@
 package uz.pdp.myeduapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity  {
     private boolean isBackPressedOnce = false;
+
+    BottomNavigationView bottomNavigationView;
 
 
     @SuppressLint("ObsoleteSdkInt")
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         LinearLayout linearLayout1;
         LinearLayout linearLayout2;
@@ -67,13 +75,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
         findViewById(R.id.navbaricon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+        NavigationView navigationView = findViewById(R.id.navBarMenu);
+        navigationView.setItemIconTintList(null);
     }
 
 
@@ -93,5 +104,50 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
 
 
+        bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        bottomNavigationView.setSelectedItemId(R.id.menuHome);
+
     }
+
+//
+    AccountFragment accountFragment = new AccountFragment();
+    HomeFragment homeFragment = new HomeFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+    FavouriteLessonsFragment favouriteLessonsFragment = new FavouriteLessonsFragment();
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+
+
+        switch (item.getItemId()) {
+            case R.id.menuHome:
+                selectedFragment=new HomeFragment();
+                break;
+
+            case R.id.menuAccount:
+                selectedFragment=new AccountFragment();
+                break;
+            case R.id.menuLike:
+                selectedFragment= new FavouriteLessonsFragment();
+                break;
+            case R.id.menuSettings:
+                selectedFragment=new SettingsFragment();
+                break;
+        }
+        assert selectedFragment != null;
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+        return true;
+
+    }
+            };
+
 }
